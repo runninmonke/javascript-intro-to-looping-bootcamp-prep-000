53b26fc3431525833d71aec99b56f4e394c3574d
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-09-07 22:50:18 +0000

    Done.

--- loops.js
+++ loops.js
@@ -4,3 +4,22 @@ function forLoop(array) {
   }
   return array;
 }
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n);
+    n--;
+  }
+  return 'done';
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+}
+
+function doWhileLoop (array) {
+  do {
+    array.pop();
+  } while(array.length > 0 && maybeTrue());
+  return array;
+}
